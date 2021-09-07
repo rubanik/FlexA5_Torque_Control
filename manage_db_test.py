fcaa28b02cd89b84a14ae0887d78fd4eab257060
rubanik <anton.rubanik@gmail.com>
2021-09-07 23:20:41 +0300

    first commit

---
+++ manage_db_test.py
@@ -0,0 +1,80 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+conn = sqlite3.connect('./FlexA5_Torque_Control/mydb.db')
+cursor = conn.cursor()
+
+try:
+    # Создание таблицы
+    cursor.execute("""CREATE TABLE data
+                    (time text, machine text, speed text,
+                    drive text, torque text,state text)
+                """)
+except:
+    print("Already Exist")
+
+
+# cursor.execute("""INSERT INTO data
+#                   VALUES ('Glow', 'Andy Hunter', '7/24/2012',
+#                   'Xplore Records', 'MP3')"""
+#                )
+ 
+# Сохраняем изменения
+conn.commit()
+ 
+# Вставляем множество данных в таблицу используя безопасный метод "?"
+data = [('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.435','130'),
+          ('10/10/2021', 'FlexA', '470', 'Wheel3Folder', '1.445','130'),
+          ('10/10/2021', 'FlexA', '470', 'CutDrum', '0.23','130'),
+          ('10/10/2021', 'FlexA', '470', 'TinFoilDrive', '1.835','130')]
+ 
+for i in range(0,1000):
+    cursor.executemany("INSERT INTO data VALUES (?,?,?,?,?,?)", data)
+
+conn.commit()
+
+print('done')
+# print("Here's a listing of all the records in the table:")
+# for row in cursor.execute("SELECT rowid, * FROM data ORDER BY machine"):
+#     print(row)
+
